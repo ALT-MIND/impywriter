2aebd7a298fd83784b682901b0bd66ec0abd24d0
Alt Mind <altmind.inb@gmail.com>
2020-12-07 09:35:11 +0000

    update source

--- image_write.py
+++ image_write.py
@@ -1,11 +1,14 @@
 import argparse
+import subprocess
 import os
 
 parser = argparse.ArgumentParser(description='Image Writer')
 
 parser.add_argument('-f', '--file' , help='File name')
 namespace = parser.parse_args()
-
 file_name = namespace.f
+q = subprocess.check_output(['hwinfo', '--disk', '--short'])
+q = q.decode('utf-8')
+
 
 os.system(f"sudo dd bs=4M if={file_name} of=/dev/sdb status=progress oflag=sync  ")
